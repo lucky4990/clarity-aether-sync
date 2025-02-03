0c1e8f606887d9771eb045fd75c855d05272320a
lucky4990 <ezekiellucky297@gmail.com>
2025-02-03 21:56:15 +0100

    fix file

--- tests/aether-sync_test.ts
+++ tests/aether-sync_test.ts
@@ -57,3 +57,40 @@ Clarinet.test({
     block.receipts[1].result.expectOk().expectUint(1);
   },
 });
+
+Clarinet.test({
+  name: "Ensure can close channel and prevent further updates",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const wallet_1 = accounts.get("wallet_1")!;
+    const wallet_2 = accounts.get("wallet_2")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "aether-sync",
+        "create-channel",
+        [types.principal(wallet_2.address)],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        "aether-sync", 
+        "close-channel",
+        [types.uint(1)],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        "aether-sync",
+        "post-update",
+        [
+          types.uint(1),
+          "0x0000000000000000000000000000000000000000000000000000000000000000",
+          "0x0000000000000000000000000000000000000000000000000000000000000000"
+        ],
+        wallet_1.address
+      )
+    ]);
+
+    assertEquals(block.receipts.length, 3);
+    block.receipts[1].result.expectOk().expectBool(true);
+    block.receipts[2].result.expectErr(100);
+  },
+});
